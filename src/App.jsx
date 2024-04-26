ce483a83e2c13119be4a9ad98457e360474762e5
romit <romitgabani1@gmail.com>
2024-04-26 21:01:48 +0530

    added performance monitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import Experience from "./experience/Experience";
+import { Perf } from "r3f-perf";
 
 function App() {
   return (
     <>
       <Canvas>
+        <Perf />
         <Experience />
       </Canvas>
     </>
